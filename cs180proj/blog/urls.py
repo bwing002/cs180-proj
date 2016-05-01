9463061d0a0a2eff9be943bab2a1b07a8ed08a82
Alex Mah <amah001@ucr.edu>
2016-05-01 04:02:57 -0700

    test commit/admin link test

--- cs180proj/blog/urls.py
+++ cs180proj/blog/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.login, name = 'login',
 ]
