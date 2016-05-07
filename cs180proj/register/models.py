50b97362403a566849665de870dd5b0afd3a6b18
atost001 <atost001@ucr.edu>
2016-05-06 18:18:57 -0700

    Adding all changes

---
+++ cs180proj/register/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Register(models.Model):
+  username = 
