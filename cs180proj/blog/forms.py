c664a3601a55f1764c4f487407f11570b97da1ab
Alex Mah <Alex Mah>
2016-05-15 05:01:51 -0700

    blog feed, edit blog, new post

--- cs180proj/blog/forms.py
+++ cs180proj/blog/forms.py
@@ -1,7 +1,10 @@
+import re
 from django import forms
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from login.models import UserProfile
 from .models import Post
-#class PostForm(forms.Modelform):
-
-#	class Meta:
-#		model = Post
-#		fields = ('title','text',)
+class PostForm(forms.ModelForm):
+		class Meta:
+			model = Post
+			fields = ('title', 'text')
--- cs180proj/blog/urls.py
+++ cs180proj/blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url, patterns, include
 from . import views
 urlpatterns = [
     url(r'^blog/$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     #url(r'^blog/$',post_list),
     #url(r'^post/new/$',views.post_new, name='post_new'),
     #url(r'^accounts/login/$', views.login, name = 'login'),
