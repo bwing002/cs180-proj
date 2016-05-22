d00397f2c2ee86269aedaeda048eb94360b4f893
Brett Wing <bwing002@tango-42.cs.ucr.edu>
2016-05-21 18:57:19 -0700

    profile picture urls

--- cs180proj/login/forms.py
+++ cs180proj/login/forms.py
@@ -30,7 +30,7 @@ class UpdateInformation(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ('first_name', 'last_name','user_sex', 'birth_date',)
+        fields = ('first_name', 'last_name','user_sex', 'birth_date','profile_picture_url',)
 
 
 #class UpdateFirstName(forms.ModelForm):
