97288ac9a20c0b019e856782e94a78e34ff506d4
Brett Wing <bwing002@tango-42.cs.ucr.edu>
2016-05-21 18:17:10 -0700

    view profiles

--- cs180proj/login/views.py
+++ cs180proj/login/views.py
@@ -107,9 +107,12 @@ def edit_profile(request):
     })
     return render_to_response('login/edit_profile.html',{'userprofile':userprofile,'form':form},RequestContext(request))            
         
-        
-        
-        
+def view_profile(request, viewusername):
+    userprofile = UserProfile.objects.get(user=User.objects.get(username=viewusername))
+#    userprofile = UserProfile.objects.get(user=User.objects.get(username=viewusername))
+    user = User.objects.get(username=viewusername)
+#    return render(request, 'login/view_profile.html', {'user':userprofile})
+    return render(request, 'login/view_profile.html', {'userprofile':userprofile,'user':user})       
         
         
         
